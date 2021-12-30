9da65a7498a44831f8b43e5622caa0a1f9bc3896
Bhakti Daraniya <180540107035@darshan.ac.in>
2021-12-30 11:32:03 +0530

    Assignment 3

---
+++ app/src/main/java/com/example/tatvasoftassignment3/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.tatvasoftassignment3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText etNum1,etNum2;
+    TextView tvResult;
+    Button btnAdd;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etNum1=(EditText)findViewById(R.id.etNum1);
+        etNum2=(EditText)findViewById(R.id.etNum2);
+        tvResult=(TextView) findViewById(R.id.tvResult);
+        btnAdd=(Button)findViewById(R.id.btnAdd);
+    }
+
+    public void btnAdd(View view) {
+
+        // debugging using log and debugger
+        int n1 = Integer.parseInt(etNum1.getText().toString());
+        Log.i("debug", "Executed n1");
+        int n2 = Integer.parseInt(etNum2.getText().toString());
+        Log.i("debug", "Executed n2");
+        int result = n1+n2;
+        Log.i("debug", "Executed add operation");
+        //tvResult.setText(result); //-----will not work
+        tvResult.setText("Result is = "+result);
+        Log.i("debug", "Executed program");
+    }
+}
